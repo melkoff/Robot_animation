d774e9e6131503fc511a6889f45aab8065e7ca2e
melkoff <vasamelko@gmail.com>
2024-03-26 17:31:58 +0200

    optimaze code

--- script.js
+++ script.js
@@ -1,78 +1,62 @@
-function showPopup(message) {
-   let popup = document.getElementById('popup');
-   let messageElement = document.getElementById('message');
-   messageElement.innerHTML = message;
-   popup.style.display = 'block';
-}
-
-// Function to close robot
-function closePopup() {
-   let popup = document.getElementById('popup');
-   popup.style.display = 'none';
-}
-
+// Start events on click call-me-button
 document.getElementById("call-me-button").addEventListener("click", function () {
+   showMessages();
    // Hide the Call Me button
    document.getElementById("call-me-button").style.display = "none";
    // Show the close button
    document.getElementById("close-button").style.display = "block";
+   // Add class active to change style
    document.querySelector(".robot-popup-container").classList.add("active");
+   // Add class active to change style
    document.querySelector(".robot-popup").classList.add("active");
-   document.querySelector(".call-robot").classList.add("active");
 });
 
+// Start events on click close-button
 document.getElementById("close-button").addEventListener("click", function () {
-   if (document.querySelector(".robot-popup-container").classList.contains("active")) {
-      document.getElementById("robot-image").style.transform = "rotate(0deg)";
-      // Show Call Me button again
-      document.getElementById("call-me-button").style.display = "block";
-      // Hide close button
-      document.getElementById("close-button").style.display = "none";
-      document.querySelector(".robot-popup-container").classList.remove("active");
-      document.querySelector(".robot-popup").classList.remove("active");
-      document.querySelector(".call-robot").classList.remove("active");
-      // Hide the robot message
-      document.querySelector(".robot-message").style.display = "none";
-   }
+   fromStart()
+   // Show Call Me button again
+   document.getElementById("call-me-button").style.display = "block";
+   // Hide close button
+   document.getElementById("close-button").style.display = "none";
+   // Remove class active to change style by default
+   document.querySelector(".robot-popup-container").classList.remove("active");
+   // Remove class active to change style by default
+   document.querySelector(".robot-popup").classList.remove("active");
 });
-
-let currentMessageIndex = 0;
-let messages = document.querySelectorAll(".robot-message");
 
 function showMessages() {
    let messages = document.querySelectorAll(".robot-message");
-   // Time to show message
-   let delay = 4000; // 4 seconds
-   let currentIndex = -1; // Start from -1 to show first message
+   let delay = 1000; // 1 секунда
+   let currentIndex = -1; // Почати з -1, щоб показати перше повідомлення
 
-   function showMessage() {
-      // Hide messages
-      for (var i = 0; i < messages.length; i++) {
-         messages[i].classList.remove("active");
+   function showOneMessage() {
+      if (currentIndex >= 0) {
+         messages[currentIndex].classList.remove("active");
       }
 
-      // Update current message index
       currentIndex++;
       if (currentIndex >= messages.length) {
-         currentIndex = 0; // Repeat messages if end
+         currentIndex = 0; // Повторювати повідомлення, якщо кінець
       }
-
-      // Move current message to the beginning of the container
 
       let container = document.querySelector(".messages-container");
       let currentMessage = messages[currentIndex];
       container.prepend(currentMessage);
-
-      // Show current message
       currentMessage.classList.add("active");
-
-      setTimeout(showMessage, delay); // Show next message after set delay
+      setTimeout(showOneMessage, delay); // Показати наступне повідомлення після затримки
    }
 
-   showMessage(); // Start showing messages
+   showOneMessage();
 }
 
-document.getElementById("call-me-button").addEventListener("click", function () {
+function fromStart() {
+   document.querySelector(".messages-container").innerHTML = "";
+   document.querySelector(".robot-message").classList.remove("active");
+   document.querySelector(".robot-message").classList.remove("active");
+   document.getElementById("call-me-button").style.display = "block";
+   document.getElementById("close-button").style.display = "none";
+   document.querySelector(".robot-popup-container").classList.remove("active");
+   document.querySelector(".robot-popup").classList.remove("active");
 
-   showMessages(); // Start showing messages
-});
+
+}
